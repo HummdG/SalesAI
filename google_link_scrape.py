f30aeb8f5701d9feb7fd670f90f5768cd0264840
Hummd <hummd2001@gmail.com>
2024-02-16 12:06:36 +0000

    quality of life improvements

--- google_link_scrape.py
+++ google_link_scrape.py
@@ -8,7 +8,7 @@ def get_news_articles(api_key, cx, treatment):
         "cx": cx,  # Custom Search Engine ID
         "key": api_key,
         "num": 10,  # Adjust the number of results per request
-        "sort": "date:r:20200101:20231231",  # Date range (adjust as needed)
+        "sort": "date:r:20150101:20231231",  # Date range (adjust as needed)
     }
 
     response = requests.get(base_url, params=params)
@@ -24,8 +24,13 @@ def get_news_articles(api_key, cx, treatment):
             snippet = item.get("snippet", "")
 
             print(f"Title: {title}\nLink: {link}\nSnippet: {snippet}\n")
+            print("##################################################")
 
     else:
         print(f"Failed to retrieve news articles. Status code: {response.status_code}")
 
-get_news_articles("AIzaSyCjOxGey7Xlb_kaNp67TvPxS-t78XymXNI", "33d3c2b2a85ff4505", "emsculpt")
+API_KEY = "AIzaSyCjOxGey7Xlb_kaNp67TvPxS-t78XymXNI"
+SEACH_ENGINE_ID = "33d3c2b2a85ff4505"
+treatment = input("Treatment Name: ")
+print("---------------------------------------------")
+get_news_articles(API_KEY, SEACH_ENGINE_ID, treatment)
